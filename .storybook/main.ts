13b12f350890843ec20d23dad28614ac8bed68b4
George Pilitsoglou <george.pilitsoglou@gmail.com>
2025-06-26 22:29:37 +0100

    chore: added storybook a11y addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
   ],
   "addons": [
     "@storybook/addon-docs",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-a11y"
   ],
   "framework": {
     "name": "@storybook/react-vite",
