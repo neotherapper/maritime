e50b3901a8fcd7167dfe96215e62860fa9006a4a
George Pilitsoglou <george.pilitsoglou@gmail.com>
2025-06-26 18:50:10 +0100

    chore: added vitest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,8 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    "@storybook/experimental-addon-test"
   ],
   "framework": {
     "name": "@storybook/react-vite",
